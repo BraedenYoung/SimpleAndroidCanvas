ab85738de070002c93d83a17842188b6e9b3bbd1
Braeden Young <braeden.w.young@gmail.com>
2017-03-26 12:48:36 -0400

    Revert "Revert "SimpleCanvas : Initial commit. Spike out simple grid with swipable actor""
    This reverts commit 1bc7b6d88e354bbfc8d2b01cadff504d68461a0f.

---
+++ java/com/example/braedenyoung/myapplication/Block.java
@@ -0,0 +1,16 @@
+package com.example.braedenyoung.myapplication;
+
+
+public class Block {
+
+    int row;
+    int col;
+
+    int color;
+
+    public Block(int row, int col, int color) {
+        this.row = row;
+        this.col = col;
+        this.color = color;
+    }
+}
